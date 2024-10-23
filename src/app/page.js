a8e3d3aa80680ef5f66d23fbca50d7d9fd405cff
Nora <nhalmarri64@gmail.com>
2024-10-23 11:04:00 +0300

    Outline

--- src/app/page.js
+++ src/app/page.js
@@ -1,28 +1,15 @@
-"use client";
-
-import React, { useState } from "react";
-import Input from "./Components/inputs";
+//import Image from "next/image";
 
 export default function Home() {
-  const [inputValue, setInputValue] = useState("");
-
-  const handleInputChange = (e) => {
-    setInputValue(e.target.value);
-  };
-
   return (
-    <div className="bg-cyan-50 min-h-screen flex items-center justify-center">
-      <div className="w-full max-w-md p-4  text-black ">
-        <label htmlFor="inputField" className="block text-black mb-2">
-          Enter something:
-        </label>
-        <Input
-          id="inputField"
-          value={inputValue}
-          onChange={handleInputChange}
-          placeholder="Type here..."
-        />
-      </div>
+    <div className="w-screen h-[90vh] flex flex-col justify-center items-center md:flex-row font-mono">
+      <h1 className="text-center text-[60px] ">Need a Buddy, Buddy</h1>
+      {/* <Image
+        src="https://i.pinimg.com/control/564x/21/b6/3e/21b63e6f58b9d7b828a09c80b87987f4.jpg"
+        alt="Buddylogo"
+        width={300}
+        height={300}
+      /> */}
     </div>
   );
 }
